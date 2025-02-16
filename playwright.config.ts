66a120a5c2d5826ce34245f4862a28ac7d1baee4
sinscrit <sinscrit@gmail.com>
2025-02-17 00:17:14 +0100

    Add files via upload

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const supportedBrowsers = {
+  chromium: devices['Desktop Chrome'],
+  firefox: devices['Desktop Firefox'],
+  webkit: devices['Desktop Safari']
+};
+
+const selectedBrowser = process.env.PLAYWRIGHT_BROWSER || 'chromium';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: selectedBrowser,
+      use: { ...supportedBrowsers[selectedBrowser] },
+    }
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 30000
+  },
+});
